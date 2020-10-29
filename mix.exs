62b3fa945c3f9656e998891ae44e3caae48d60af
Tom Buscher <tom@storycloud.co>
2020-10-29 14:49:30 -0700

    Update ex_aws dependency

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ExAwsCognitoIdentity.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_aws, "~> 2.0.0"},
+      {:ex_aws, "~> 2.1"},
       {:hackney, ">= 0.0.0", only: [:dev, :test]},
       {:sweet_xml, ">= 0.0.0", only: [:dev, :test]},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
